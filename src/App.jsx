3c9d1ed3045fbf32002418434af07810612fdcb4
nanoha <nanoha.creator@gmail.com>
2023-01-21 16:53:50 +0000

    create functional component (Chats, Chat)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import defaultDataset from "./dataset";
-import {AnswersList} from "./components/index";
+import { AnswersList, Chats } from "./components/index";
 import "./assets/styles/style.css";
 
 export default class App extends React.Component {
@@ -15,24 +15,43 @@ export default class App extends React.Component {
     };
   }
 
-  initAnswer = () =>{
+  initAnswer = () => {
     const initDataset = this.state.dataset[this.state.currentId];
     const initAnswers = initDataset.answers;
     this.setState({
-      answers: initAnswers
-    })
-  }
+      answers: initAnswers,
+    });
+  };
 
-  componentDidMount(){
+  initChats = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const chat = {
+      text: initDataset.question,
+      type: "question",
+    };
+
+    const chats = this.state.chats;
+    if (chats.length === 0) {
+      chats.push(chat);
+    }
+
+    this.setState({
+      chats: chats,
+    });
+  };
+
+  componentDidMount() {
     // 初期化後、データをセット
-    this.initAnswer()
+    this.initChats();
+    this.initAnswer();
   }
 
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          <AnswersList answers={this.state.answers}/>
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} />
         </div>
       </section>
     );
